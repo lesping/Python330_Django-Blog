9e1fb8805e516f9078c2ff404c981cbae2224bfa
lesping <lespin2@uw.edu>
2024-07-06 12:21:43 -0400

    Fixing the class

--- blogging/models.py
+++ blogging/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Poll(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
