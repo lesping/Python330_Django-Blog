ad1a06f41748afc293dd8410388dabf651d63088
lesping <lespin2@uw.edu>
2024-07-06 12:12:32 -0400

    Fixing the blogging models file

--- blogging/models.py
+++ blogging/models.py
@@ -1,14 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Poll(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
-    author = models.CharField(max_length=60)
-    created_date = models.DateField()
-    modified_date = models.DateField()
-    published_date = models.DateField()
-
-    def __str__(self):
-        return self.title
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+    published_date = models.DateField(blank=True, null=True)
