fcf833d531841f3bcdb9ba638ef98ea6ad98a0c1
lesping <lespin2@uw.edu>
2024-07-11 20:14:43 -0400

    Modifying admin file

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+# Customized ModelAdmin class for Post and Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
