306ce06727745dbbb97dead8e1a7e975c310d896
lesping <lespin2@uw.edu>
2024-07-27 20:02:42 -0400

    Polling Class Based Views

--- polling/urls.py
+++ polling/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import list_view, detail_view
+from .views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    path('', PollListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>/', PollDetailView.as_view(), name="poll_detail"),
 ]
