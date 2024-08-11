dcc3ad2f9424a8fb4bf88ad49b4dc5bb9fc39831
lesping <lespin2@uw.edu>
2024-08-11 13:07:30 -0400

    fixing travis issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
